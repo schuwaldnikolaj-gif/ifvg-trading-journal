f1aa732c76b6ff7b689874ebf973c3aacb770908
schuwaldnikolaj-gif <schuwald.nikolaj@googlemail.com>
2026-08-26 22:14:20 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
-const CACHE="ifvg-journal-v3";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="ifvg-journal-v6";
+const ASSETS=["./","./index.html","./manifest.json","./sw.js"];
+self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET") return;
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone();
+    if(new URL(event.request.url).origin===location.origin){caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});}
+    return response;
+  }).catch(()=>caches.match(event.request).then(r=>r||caches.match("./index.html"))));
+});
